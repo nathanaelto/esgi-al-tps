c5d350a4f267bf33bf33d2629f70ece19c117b75
Grégory BOISSINOT <gregory.boissinot@ratpsmartsystems.com>
2021-10-27 16:34:24 +0200

    Enhancement

---
+++ tp10/src/main/java/fr/esgi/tp10/MainTestRefexion.java
@@ -0,0 +1,15 @@
+package fr.esgi.tp10;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class MainTestRefexion {
+
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        final Class<LoggerFactory> loggerFactoryClass = LoggerFactory.class;
+        final Constructor<LoggerFactory> declaredConstructor = loggerFactoryClass.getDeclaredConstructor();
+        declaredConstructor.setAccessible(true);
+        final LoggerFactory loggerFactory = declaredConstructor.newInstance();
+        System.out.println(loggerFactory!=null);
+    }
+}
